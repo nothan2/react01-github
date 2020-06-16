4e7e6faf657b044033f3340581f28139008a7797
nothan2 <nothan@naver.com>
2020-06-16 22:04:59 +0900

    test

--- src/index.js
+++ src/index.js
@@ -11,6 +11,15 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+// 01. Arror Function
+const hello = msg => "Hello " + msg;
+const han = hello("han");
+console.log(han);
+
+const message = (name = "han2") => "hello" + name;
+const testmsg = message();
+console.log(testmsg);
+
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
